71680869bc8c5a24f731c80fa57b2857670a721c
Saima Imran <smalldeeds1234@gmail.com>
2024-09-01 02:51:33 +0500

    Add files via upload

---
+++ js assigement 4/app.js
@@ -0,0 +1,225 @@
+// Quetion #1//------------------------------------------------------------------------------
+
+// var Karachi=("the city of lights")
+// var city=prompt("Your city")
+// document.write("Welcome to" + Karachi)
+
+// Quetion #2//------------------------------------------------------------------------------
+
+// var gender =prompt("Type your gender")
+// if (gender === "male"){
+//     document.write("Good Morning Sir")
+// }
+// else{
+//     document.write("Good Morning Ma'am")
+// }
+
+// Quetion #3//------------------------------------------------------------------------------
+
+// var Red=("Must Stop")
+// var Yellow=("Ready to move")
+// var Green=("Move Now")
+
+// var signal=prompt("Type your signal color")
+// var signal=prompt("Type your signal color")
+// var signal=prompt("Type your signal color")
+
+// document.write("<b>Signal color</b>"+"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+"<b>Message</b></br>")
+// document.write( "Red"+"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+Red+"</br>")
+// document.write("Yellow"+"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+Yellow+"</br>")
+// document.write("Green"+"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+Green+"</br>")
+
+// Quetion #4//----------------------------------------------------------------------------------
+
+
+// var remaining_fuel_in_car=prompt("Fuel in litre")
+// if (remaining_fuel_in_car<=0.25){document.write("Please refill the fuel in car")
+
+// }else
+
+// {document.write(remaining_fuel_in_car+"litre")}
+
+// Quetion #5 a//----------------------------------------------------------------------------------
+
+// var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+// It is displayed.
+
+// ------b--------//
+//  var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+
+// Its not displayed.//
+
+// ------c-------//
+
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+
+// 1 and 3 are not displayed ,2 and 4 are displayed.//
+
+// ---------d-------------//
+
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+
+// It is displayed//
+
+// -------e--------------//
+
+// if (true){
+//     alert("True");
+//     }
+//     if (false){
+//     alert("False");
+//     }
+
+//     Its is displayed
+
+// -------f-----------//
+// if("car" < "cat"){
+//     alert("car is smaller than cat");
+//     }
+    // It is displayed.//
+
+    // Quetion #6 //----------------------------------------------------------------------------------
+  
+    // var Total_marks=(300)
+    //  var Percentage=(obtained_marks/Total_marks*100)
+    //  var obtained_marks=prompt("Marks obtained")
+
+    
+    // if((obtained_marks/Total_marks*100)>=70){
+    //     document.write("<h1>Mark Sheet</h1></br>");
+    //     document.write("Total Marks"+":"+Total_marks +"</br>");
+    //     document.write("Marks Obtained"+":"+obtained_marks+"</br>");
+    //     document.write("Percentage"+":"+obtained_marks/Total_marks*100+"%"+"</br>");
+    //     document.write("Grade"+":"+"<b>A</b>"+"</br>");
+    //     document.write("Remarks"+":"+"Good" +"</br>");}
+
+    // if ((obtained_marks/Total_marks*100)>=80){
+    //     document.write("<h1>Mark Sheet</h1></br>")
+    //     document.write("Total Marks"+":"+Total_marks +"</br>")
+    //     document.write("Marks Obtained"+":"+obtained_marks+"</br>")
+    //     document.write("Percentage"+":"+obtained_marks/Total_marks*100+"%"+"</br>")
+    //     document.write("Grade"+":"+"<b>A-One</b>"+"</br>")
+    //     document.write("Remarks"+":"+"Excellent" +"</br>")}
+    
+    // if((obtained_marks/Total_marks*100)>=60){
+    //     document.write("<h1>Mark Sheet</h1></br>")
+    //     document.write("Total Marks"+":"+Total_marks +"</br>")
+    //     document.write("Marks Obtained"+":"+obtained_marks+ "</br>")
+    //     document.write("Percentage"+":"+obtained_marks/Total_marks*100+"%"+"</br>")
+    //     document.write("Grade"+":"+"<b>B</b>"+"</br>")
+    //     document.write("Remarks"+":"+"You need to Improve"+"</br>")
+
+    // }else
+    //     {document.write("<h1>Mark Sheet</h1></br>")
+    //     document.write("Total Marks"+":"+Total_marks +"</br>")
+    //     document.write("Marks Obtained"+":"+obtained_marks+"</br>")
+    //     document.write("Percentage"+":"+obtained_marks/Total_marks*100+"%"+"</br>")
+    //     document.write("Grade"+":"+"<b>Fail</b>"+"</br>")
+    //     document.write("Remarks"+":"+"Sorry" +"</br>")}
+
+// Quetion #7//------------------------------------------------------------------------------
+
+
+// var secret_num=(5);
+// var guess=prompt("Guess the secret number")
+// if(guess==5){
+//     document.write( "Bingo! Correct answer")
+// }else
+// {
+//     document.write("Close enough to the correct answer")
+// }
+
+// Quetion #8//------------------------------------------------------------------------------
+
+// var num1=prompt("write a number")
+// var num2=prompt("write a number")
+
+// if(ans===num1/num2){
+// document.write("This number is visible by 3")}
+
+// else{
+
+// }
+
+// Quetion #9//------------------------------------------------------------------------------
+
+// var even=(2,4,6,8,10)
+// var number=prompt("Write a number")
+// if(number==2,4,6,8,10){
+//     document.write("Even number")}
+// else{
+//     document.write("Odd number")
+// }
+
+// Quetion #10//------------------------------------------------------------------------------
+
+// var Temperature=(T)
+// var T=prompt("Temperature")
+
+// if (T>=40){
+//     document.write("It is too hot outside.")
+// }
+
+// if (T>=30){
+//     document.write("The Weather today is Normal.")
+// }
+
+// if (T>=20){
+//     document.write("Today’s Weather is cool.")
+// }
+
+// if (T>=10){
+//     document.write("OMG! Today’s weather is so Cool.")
+// }
+// else{
+//     document.write("Its normal")
+// }
+
+// Quetion #11//------------------------------------------------------------------------------
+
+
+
+// var num1=prompt("First number")
+// var num2=prompt("Second number")
+
+// if (num1+num2){
+//     document.write("Addition" +"="(num1+num2)+"</br>")
+// }
+
+// if(num1-num2){
+//     document.write("Subtraction" +"="(num1-num2)+"</br>")
+// }
+// if(num1*num2){
+//     document.write("Multiplication" +"="(num1*num2)+"</br>")
+// }
+// if(num1/num2){
+//     document.write("Division" +"="(num1/num2)+"</br>")
+// }
+
+// if(num1/num2*100){
+//     document.write("Modolus" +"="(num1/num2*100)+"</br>")
+
+// }
